d97cbd31d33eb94122a5981e8a35475282f80626
Dumur Alexandre <alexandre.dumur@epfl.ch>
2017-12-21 01:26:48 +0100

    modularized the code to use them more easily

--- cooc.py
+++ cooc.py
@@ -5,23 +5,28 @@ import pickle
 from nltk import bigrams, trigrams
 
 
-def main():
-    with open('vocab_full.pkl', 'rb') as f:
+def to_cook(path_vocab = 'vocab_full.pkl', path_tweets_pos = "clean_pos_bitri=True", path_tweets_neg = "clean_neg_bitri=True",path_tweets_test ="clean_test_bitri=True" dest = 'cooc_full.pkl'):
+    """This method takes optionally the path of the pickled vocab
+    and the path of the tweets. It then build with them to co-ocurrence matrix
+    write the result in the file dest
+    """
+
+    with open(path_vocab, 'rb') as f:
         vocab = pickle.load(f)
     vocab_size = len(vocab)
 
     data, row, col = [], [], []
     counter = 1
-    for fn in ['preprocessed_pos', 'preprocessed_neg']:
+    for fn in [path_tweets_pos, path_tweets_neg, path_tweets_test]:
         with open(fn) as f:
             for line in f:
                 tokens = line.strip().split()
-                
+
                 def generator(unigrams):
                     yield from unigrams
                     yield from bigrams(unigrams)
                     yield from trigrams(unigrams)
-               
+
                 tokens_ids = [vocab.get(t, -1) for t in generator(tokens)]
                 tokens_ids = [t for t in tokens_ids if t >= 0]
                 for t in tokens_ids:
@@ -37,9 +42,5 @@ def main():
     cooc = coo_matrix((data, (row, col)))
     print("summing duplicates (this can take a while)")
     cooc.sum_duplicates()
-    with open('cooc_full.pkl', 'wb') as f:
+    with open(dest, 'wb') as f:
         pickle.dump(cooc, f, pickle.HIGHEST_PROTOCOL)
-
-
-if __name__ == '__main__':
-    main()
